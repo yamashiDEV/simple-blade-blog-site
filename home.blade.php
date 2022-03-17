e2b78b8d5ba3c54a3a61bb66cbf29efd30e20f60
yamashiDEV <94204255+yamashiDEV@users.noreply.github.com>
2022-03-17 09:38:50 +0100

    Add files via upload

---
+++ home.blade.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital@1&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@1,375;1,480&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@200&family=Rubik:ital,wght@1,375;1,480&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>@include('navbar') </header>
+
+    <div class="gory">
+        <h1>Wycieczka z tigerem</h1>
+    </div>
+    <div class="posty">
+        <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit delectus similique eligendi reiciendis ipsa necessitatibus, maxime officia exercitationem consequuntur nihil reprehenderit ab, dignissimos qui asperiores maiores doloribus sed nostrum illo! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, voluptatibus vero amet dicta explicabo temporibus maxime excepturi qui cupiditate magnam architecto blanditiis cum enim? Vitae aspernatur maxime dolore harum libero. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio architecto, impedit porro quaerat blanditiis aliquam velit veniam, eveniet vero similique ex iste quis voluptatum consectetur repudiandae temporibus? Harum, dolor iste? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro facilis ad beatae aut ut, eum, iste, exercitationem optio voluptatum nisi sint incidunt suscipit? Nihil nobis quo eius nostrum molestiae repellendus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusantium perspiciatis quam impedit, delectus enim voluptates velit optio fugit nesciunt autem dolorem illum facilis adipisci ad magni aspernatur, vel quidem! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus illum cum iure. Quos minima et, quaerat dolor veritatis quasi asperiores repellendus error quod praesentium, velit illo nesciunt voluptatem magni hic? Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint similique quos doloribus necessitatibus iusto nostrum tempore nemo consequatur dolor tenetur ipsum architecto ea, dolore porro repellat blanditiis nulla! Doloribus, atque. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus eum blanditiis saepe enim ducimus, incidunt error iure eaque quam quibusdam ipsam repellat deleniti corrupti ut, officia, velit reiciendis ea explicabo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam ullam placeat natus repellat perspiciatis officiis illum, pariatur iure corrupti! Vitae aliquid aut consectetur velit qui numquam necessitatibus soluta? Dignissimos! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, ab magnam consectetur pariatur ratione dicta, cum minima natus beatae explicabo assumenda, totam nulla quisquam optio! Natus ut nihil inventore eligendi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Et exercitationem expedita repellat. Eligendi asperiores in beatae, adipisci culpa nostrum vitae repudiandae debitis soluta labore laboriosam iste. Quam explicabo aspernatur id? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia error ea blanditiis natus cum similique sequi nostrum praesentium beatae labore consequatur enim quam laboriosam, quibusdam, doloremque eum laudantium aliquid ducimus? lorem</h5>
+        <img src="{{ asset('storage/images/panda.png') }}" alt="" width="30%" height="30%">
+    </div>
+<footer>@include('footer')
+</footer>
+
+
+
+</body>
+</html>
